29aa8d8c5c5b282cf04d3df0ae783df47b399180
Diego Neves <pdcassiano@gmail.com>
2022-08-21 22:01:31 -0300

    Funções

---
+++ Frontend/DevQuest/javascript-basico/09-funcoes/index.js
@@ -0,0 +1,12 @@
+function media(a , b) {
+    console.log('A média é: ', (a+b)/2)
+}
+
+media(5, 15)
+
+function incentivarQuester(nomeQuester = "Quester"){
+    console.log("Muito bem " + nomeQuester + " você chegou ao JS, continue assim!")
+}
+
+incentivarQuester()
+incentivarQuester("Diego")
