0812afd041138b735ff87442fe160bb0ddea5adb
Diego Neves <pdcassiano@gmail.com>
2022-08-22 21:27:21 -0300

    javascript intermediário - renomeado laddinpage

---
+++ Frontend/DevQuest/javascript-intermediario/01-escopo-no-javascript/indes.js
@@ -0,0 +1,23 @@
+// if(true){
+//     const mensagem = 'Olá'
+//     console.log(mensagem)
+// }
+// console.log(mensagem)
+
+// {
+//     var mensagem = 'Olá 1'
+//     console.log(mensagem)
+// }
+// console.log(mensagem)
+
+function falar(){
+    var mensagem = 'Olá Mundo!'
+    console.log(mensagem)
+    function falarOutraCoisa(){
+
+    }
+    falarOutraCoisa()
+    console.log(falarOutraCoisa)
+}
+falar()
+console.log(falarOutraCoisa) 
---
+++ Frontend/DevQuest/javascript-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,13 @@
+// let soma = (numero1 , numero2) => {
+//     return numero1 + numero2
+// }
+
+// let soma = (numero1 , numero2) => numero1 + numero2
+
+// console.log(soma(12,5))
+
+const incentivarQuester = nomeQuester => {
+    console.log('Parabéns ' + nomeQuester + ', Você chegou ao módulo de JS Intermediário' )
+}
+
+incentivarQuester('Diego')
