7acc4add14c507e68918eedd7cf6c0f9195047d2
Diego Neves <pdcassiano@gmail.com>
2022-08-26 16:51:41 -0300

    filter

---
+++ Frontend/DevQuest/javascript-avancado/01-filter/index.js
@@ -0,0 +1,19 @@
+let pessoas = [
+    {nome: 'Roberto', idade: 33},
+    {nome: 'Ricardo', idade: 33},
+    {nome: 'Raphael', idade: 27}
+]
+
+// let pessoasComIdadeDe33Anos = []
+
+// for (let i = 0; i < pessoas.length; i++) {
+//     if(pessoas[i].idade === 33) {
+//         pessoasComIdadeDe33Anos.push(pessoas[i])
+//     }
+// }
+
+let pessoasComIdadeDe33Anos = pessoas.filter(function(pessoa){
+    return pessoa.idade === 33
+})
+
+console.log(pessoasComIdadeDe33Anos)
