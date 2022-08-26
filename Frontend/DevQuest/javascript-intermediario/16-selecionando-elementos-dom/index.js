6542c6cf2d6fc94fd58baa7b16fa3316cefc3c7e
Diego Neves <pdcassiano@gmail.com>
2022-08-26 11:43:05 -0300

    manipulação do DOM

---
+++ Frontend/DevQuest/javascript-intermediario/16-selecionando-elementos-dom/index.js
@@ -0,0 +1,17 @@
+// console.log(document)
+
+// let p = document.getElementById('paragrafo1')
+
+// console.log(p)
+
+// let inputsPorClasse = document.getElementsByClassName('tag-de-input')
+
+// console.log(inputsPorClasse)
+// console.log(inputsPorClasse[0])
+
+// let inputPorTag = document.getElementsByTagName('input')
+// console.log(inputPorTag)
+
+let inputPorNome = document.querySelectorAll("input[name='email'")
+
+console.log(inputPorNome)
