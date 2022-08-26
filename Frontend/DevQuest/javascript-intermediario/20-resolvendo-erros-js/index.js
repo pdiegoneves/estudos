eae31309755a05c519d0695f9424386e4afc0d35
Diego Neves <pdcassiano@gmail.com>
2022-08-26 16:33:03 -0300

    conculsão do javascript intermediário

---
+++ Frontend/DevQuest/javascript-intermediario/20-resolvendo-erros-js/index.js
@@ -0,0 +1,12 @@
+let valorBoleto = 100
+
+console.log(valorBoleto)
+// console.log(valorBolet)
+
+let inputTelefone = document.getElementById('telefone')
+
+inputTelefone.disabled = true
+
+function habilitarTelefone() {
+    
+}
