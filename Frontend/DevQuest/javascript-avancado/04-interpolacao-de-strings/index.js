dd92c02f682439c91b70669aa8202566d197e215
Diego Neves <pdcassiano@gmail.com>
2022-08-26 17:45:20 -0300

    interpolação de strings

---
+++ Frontend/DevQuest/javascript-avancado/04-interpolacao-de-strings/index.js
@@ -0,0 +1,12 @@
+// const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+//     return "Primeiro Nome: " + primeiroNome + ", Último Nome: " + ultimoNome + ", Idade " + idade 
+// }
+
+// console.log(imprimirInformacoesPessoa('Roberto', 'Dias', 33))
+
+
+
+const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => {
+    return `Primeiro Nome: ${primeiroNome}, Último Nome: ${ultimoNome}, Idade: ${idade}`
+}
+console.log(imprimirInformacoesPessoa('Roberto', 'Dias', 33))
